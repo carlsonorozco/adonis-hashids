cfa1d29971ea7deda7c732fbc4b5caa556ed7c92
Carlson Orozco <carlsonorozco@gmail.com>
2016-10-19 01:35:30 +0800

    Add Commands Provider

---
+++ providers/CommandsProvider.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * adonis-hashids
+ *
+ * (c) Carlson Orozco <carlsonorozco@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+const ServiceProvider = require('adonis-fold').ServiceProvider
+
+class CommandsProvider extends ServiceProvider {
+
+  * register () {
+    this.app.bind(`Adonis/Commands/Hashids:Config`, (app) => {
+      const Helpers = app.use('Adonis/Src/Helpers')
+      const Generator = require(`../src/Commands/ConfigGenerator`)
+      return new Generator(Helpers)
+    })
+  }
+}
+
+module.exports = CommandsProvider
